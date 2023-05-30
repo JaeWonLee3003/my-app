c7c1af66a95d5f24a8ab24160336e344c99d6ddd
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-30 15:25:35 +0900

    cra-todolist

---
+++ src/App.styled.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+display:flex;
+background-color: #fcfcfc;
+flex-direction: column;
+align-items: center;
+`
+export const RowBox = styled.div `
+display:flex;
+margin: 30px 0px;
+width: 200px;
+border-radius: 70px;
+
+`
+export const Input = styled.input `
+`
+export const Button = styled.button `
+`
+export const List = styled.ul `
+display: flex;
+flex-direction: column-reverse;
+
+`
+export const Todo = styled.li `
+font-family: 'MBC1961GulimM';
+display: flex;
+list-style: none;
+align-items: center;
+border-radius: 100px;
+border: 2px black solid;
+margin: 10px 0px;
+padding: 0px 0px 0px 20px;
+width: 550px;
+height: 30px;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import type { ChangeEvent , KeyboardEvent } from 'react'
 import './App.css';
+import { Container ,Button, RowBox, Input, List, Todo} from './App.styled';
+
+interface Todo{
+  id: number,
+  name: string
+}
 
 function App() {
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [TodoName, setTodoName] = useState<string>('');
+
+  const handleInputChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setTodoName(value)
+  }
+
+  const handlePressEnter = ({ key }: KeyboardEvent<HTMLInputElement>) => {
+    if ( key === 'Enter') addTodo();
+
+  }
+
+  // page 내의  state가 변경 되었을 때
+  // props가 변경 되었을 때
+
+  const addTodo = () => {
+    if(!TodoName.trim()) return;
+    setTodos(prevState => [...prevState, {id: prevState.length , name: TodoName}]);
+    setTodoName('');
+  }
+
+
+  useEffect(() => {
+    try {
+      const parseData = JSON.parse(localStorage.getItem('todos') || '')
+      setTodos(parseData)
+    }catch(error){
+      console.log(error);
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<Container>
+  <RowBox>
+    <Input placeholder='Todo를 입력해주세요.' value={TodoName} onChange={handleInputChange} onKeyUp={handlePressEnter}/>
+    <Button onClick={addTodo}>추가</Button>
+  </RowBox>
+  <List>
+    {todos.map(({id,name}) => {
+      return <Todo key={id}>{name}</Todo>
+    })}
+  </List>
+</Container>
+
+    );
 }
 
 export default App;
