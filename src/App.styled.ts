c170a8cb08e2ed7f040f9616d8d457d1a3bd63a7
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-31 11:13:59 +0900

    2023_05_31_end

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,36 +1,53 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
+export const Wrap = styled.div `
+width: 100%;
+height: 100%;
+flex-direction: column;
+font-family: 'TheJamsil5Bold';
 display:flex;
-background-color: #fcfcfc;
+align-items: center;
+justify-content: center;
+background: linear-gradient(rgb(60, 135, 237), rgb(90, 184, 246));
+`
+
+export const Container = styled.div `
+width: 800px;
+height: 1300px;
+display:flex;
 flex-direction: column;
 align-items: center;
+background: white;
 `
 export const RowBox = styled.div `
 display:flex;
 margin: 30px 0px;
-width: 200px;
-border-radius: 70px;
+width: 400px;
 
 `
 export const Input = styled.input `
+width: 150px;
+height: 50px;
 `
 export const Button = styled.button `
+border-radius: 20px;
+border: 2px black solid;
 `
 export const List = styled.ul `
 display: flex;
 flex-direction: column-reverse;
+justify-content: between;
 
 `
 export const Todo = styled.li `
-font-family: 'MBC1961GulimM';
+font-family: 'TheJamsil5Bold';
 display: flex;
 list-style: none;
 align-items: center;
-border-radius: 100px;
+border-radius: 20px;
 border: 2px black solid;
 margin: 10px 0px;
 padding: 0px 0px 0px 20px;
 width: 550px;
-height: 30px;
+height: 50px;
 `
--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,123 @@
-import { useEffect, useState } from 'react';
-import type { ChangeEvent , KeyboardEvent } from 'react'
-import './App.css';
-import { Container ,Button, RowBox, Input, List, Todo} from './App.styled';
+import { useEffect, useState } from "react";
+import type { ChangeEvent, KeyboardEvent } from "react";
+import "./App.css";
+import { Container, Button, RowBox, Input, List, Todo,Wrap } from "./App.styled";
 
-interface Todo{
-  id: number,
-  name: string
+interface Todo {
+  id: number;
+  name: string;
 }
 
 function App() {
-
+  
+  // todolist data를 저장한 state
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [TodoName, setTodoName] = useState<string>('');
 
-  const handleInputChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
-    setTodoName(value)
-  }
+  // todo를 추가하기 위해 적는 내용을 담는 state
+    const [TodoName, setTodoName] = useState<string>("");
+  
+  // 현재 수정중인 todo의 id를 담는 state
+  const [editId, setEditId] = useState<number | undefined>();
 
+  // 수정 버튼을 누른 뒤 나온 input의 값을 수정하는데에 써야하기 때문에 State에 담아 놓는다.
+  const [editName, setEditName] = useState<string>('');
+
+  // todoName stat를 input의 onChange에서 setState 해주는 함수
+  const handleInputChange = ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setTodoName(value);
+  };
+
+  // todo를 만드는 input에서 enter를 눌렀을 때 todo를 추가해주는 함수
   const handlePressEnter = ({ key }: KeyboardEvent<HTMLInputElement>) => {
-    if ( key === 'Enter') addTodo();
+    if (key === "Enter") addTodo();
+  };
 
-  }
-
-  // page 내의  state가 변경 되었을 때
-  // props가 변경 되었을 때
-
+  // todo를 추가 해주는 함수
   const addTodo = () => {
-    if(!TodoName.trim()) return;
-    setTodos(prevState => [...prevState, {id: prevState.length , name: TodoName}]);
-    setTodoName('');
-  }
+    // todoName이 반값이면 추가하지 않음 (이후 코드 실행 X)
+    if (!TodoName.trim()) return;
 
+    // localStorage에서 count를 +1 해줌 (id로 사용하기 위해)
+    const count = Number(localStorage.getItem("count")) + 1;
+    localStorage.setItem("count", `$(count)`);
 
+    // todo에 새로운 todo 추가
+    setTodos((prevState) => [...prevState,{ id: prevState.length, name: TodoName }]);
+
+    // todoName을
+    setTodoName("");
+  };
+
+  // todos를 초기화 해주는 함수 (state, localStorage)
+  const resetTodos = () => {
+    setTodos([]);
+    localStorage.setItem("count", "0");
+  };
+
+  const handleEditName = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setEditName(value)
+  };
+
+  // 컴포넌트(페이지)가 "처음" 렌더링 됐을 ㄸ localStorage의 todos를 state에 넣어줌
   useEffect(() => {
+    // JSON.~ 함수는 에러가 나면 그냥 터져버리기 때문에 try/catch로 감싸줌
     try {
-      const parseData = JSON.parse(localStorage.getItem('todos') || '')
-      setTodos(parseData)
-    }catch(error){
+      const parseData = JSON.parse(localStorage.getItem("todos") || "");
+      setTodos(parseData);
+    } catch (error) {
       console.log(error);
     }
-  }, [])
+  }, []);
 
+  // state에 todos가 변화 될 때(추가, 삭제) localStorage에도 저장해줌
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
-  
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-<Container>
-  <RowBox>
-    <Input placeholder='Todo를 입력해주세요.' value={TodoName} onChange={handleInputChange} onKeyUp={handlePressEnter}/>
-    <Button onClick={addTodo}>추가</Button>
-  </RowBox>
-  <List>
-    {todos.map(({id,name}) => {
-      return <Todo key={id}>{name}</Todo>
-    })}
-  </List>
+    <Wrap>
+<h1> ToDoList</h1>
+    <Container>
+      <RowBox>
+        <Input
+          placeholder="Todo를 입력해주세요."
+          value={TodoName}
+          onChange={handleInputChange}
+          onKeyUp={handlePressEnter}
+        />
+        <Button onClick={addTodo}>추가</Button>
+        <Button onClick={resetTodos}>초기화</Button>
+      </RowBox>
+      <List>
+        {todos.map(({ id, name }) => {
+          const isEdit: boolean = editId === id;
+
+          const deleteTodo = () => {setTodos((prevState) => prevState.filter((v) => v.id !== id));};
+
+          const toggleEditTodo = () => setEditId(isEdit ? undefined : id);
+
+          const editTodo = () => {
+            setTodos(prevState => 
+              prevState.map(todo => (todo.id === id ? {...todo, name: editName } : todo)),
+            );
+            toggleEditTodo();
+          };
+
+          return (
+            <Todo key={id}>
+              {isEdit ? <input onChange={handleEditName} defaultValue={name} /> : name}
+              <Button onClick={deleteTodo}>삭제</Button>
+
+              <Button onClick={toggleEditTodo}>{isEdit ? '취소' : '수정'}</Button>
+              {isEdit && <Button onClick={editTodo}>저장</Button>}
+            </Todo>
+          );
+        })}
+      </List>
 </Container>
+    </Wrap>
 
     );
 }
